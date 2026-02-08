e0475f40cd6796f26c774b90aa0bac56a694ad4e
CodeItQuick <evanontario@hotmail.com>
2026-02-08 09:07:31 -0400

    refactor

--- src/theVault.js
+++ src/theVault.js
@@ -1,14 +1,14 @@
-﻿
-function theSafe() {
+﻿function theSafe() {
     let currentDialPosition = 50;
     let currentPassword = 0;
+
     function dial(turns) {
+        let rotation = +turns.substring(1)
+        if (rotation > 99) {
+            currentPassword = currentPassword + Math.floor(rotation / 100);
+            rotation = rotation % 100;
+        }
         if (turns.startsWith('L')) {
-            let rotation = +turns.substring(1)
-            if (rotation > 99) {
-                currentPassword = currentPassword + Math.floor(rotation / 100);
-                rotation = rotation % 100;
-            }
             if (rotation > currentDialPosition) {
                 if (currentDialPosition !== 0) {
                     currentPassword++;
@@ -17,36 +17,24 @@ function theSafe() {
                 return currentDialPosition
             } else {
                 currentDialPosition = currentDialPosition - rotation
-                if (currentDialPosition === 0) {
-                    currentPassword++;
-                }
-                return currentDialPosition;
             }
-        } else if (turns.startsWith('R')) {
-            let rotation = +turns.substring(1)
-            if (rotation > 99) {
-                currentPassword = currentPassword + Math.floor(rotation / 100);
-                rotation = rotation % 100;
+        } else if (rotation + currentDialPosition >= 100) {
+            currentDialPosition = rotation + currentDialPosition - 100;
+            currentPassword++;
+            if (currentDialPosition === 100) {
+                return currentDialPosition = 0;
             }
-            if (rotation + currentDialPosition >= 100) {
-                currentDialPosition = currentDialPosition + rotation - 100;
-                currentPassword++;
-                if (currentDialPosition === 100) {
-                    return currentDialPosition = 0;
-                }
-                return currentDialPosition
-            } else {
-                currentDialPosition = currentDialPosition + rotation;
-                if (currentDialPosition === 0) {
-                    currentPassword++;
-                }
-                return currentDialPosition;
-            }
+            return currentDialPosition
+        } else {
+            currentDialPosition = currentDialPosition + rotation;
         }
-
-        return 0;
+        if (currentDialPosition === 0) {
+            currentPassword++;
+        }
+        return currentDialPosition;
     }
-    return { dial, password: () => currentPassword, dialPosition: () => currentDialPosition };
+
+    return {dial, password: () => currentPassword, dialPosition: () => currentDialPosition};
 }
 
 export default theSafe;
